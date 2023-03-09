b7ccdbee65d7e918eeb8e9d44da48ac9e2e90528
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2023-03-09 10:51:58 -0300

    feature 3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const readJsonData = require('./utils/fs/readJsonData');
 
 const app = express();
+app.use(express.json());
+
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const movies = await readJsonData('./src/movies.json');
+
+  const findMovie = movies.find((movie) => movie.id === Number(id));
+
+  if (!findMovie) {
+      return res.status(404).json({ message: 'Filme não encontrado' });
+    }
+  return res.status(200).json(findMovie);
+});
 
 module.exports = app;
